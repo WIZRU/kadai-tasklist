fbdd225edf4d658b79cbe03daaf13ea42530a2d6
ko.takeuchi <takeuchi.ko@a-tm.co.jp>
2019-05-22 04:57:30 +0000

    make tasklist ver1.0

---
+++ resources/views/tasks/index.blade.php
@@ -0,0 +1,28 @@
+@extends('layouts.app')
+
+@section('content')
+
+<h1>残タスク一覧</h1>
+
+    @if (count($tasks) > 0)
+        <table class="table table-striped">
+            <thead>
+                <tr>
+                    <th>id</th>
+                    <th>残タスク</th>
+                </tr>
+            </thead>
+            <tbody>
+                @foreach ($tasks as $task)
+                <tr>
+                    <td>{!! link_to_route('tasks.show', $task->id, ['id' => $task->id]) !!}</td>
+                    <td>{{ $task->content }}</td>
+                </tr>
+                @endforeach
+            </tbody>
+        </table>
+    @endif
+
+    {!! link_to_route('tasks.create', '残タスクの追加', null, ['class' => 'btn btn-primary']) !!}
+
+@endsection
